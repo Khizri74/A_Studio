4de77e17d7cc4160bfa5b32eb040cdd2bb33c961
Khizri <https://github.com/Khizru74>
2026-04-14 12:55:31 +0300

    add Video file

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 const showImg = document.querySelectorAll(".avatar");
 const bigImg = document.querySelectorAll(".bigImg");
 const userProf = document.querySelectorAll(".profile");
-const flipp = document.querySelectorAll(".elipsFlip");
+const flipp = document.querySelectorAll(".ellipsFlip");
 
 const clickElements = [showImg, flipp];
 
 clickElements.forEach((nodeList) =>
   nodeList.forEach((elem, index) => {
     elem.addEventListener("click", () => {
-      console.log("Клик по картинке $", index);
       userProf.forEach((profile) => {
         profile.style.display = "none";
       });
@@ -25,7 +24,30 @@ avatars.forEach(avatar => {
   avatar.addEventListener('click', () => {
     avatars.forEach(a => a.classList.remove('focused'));
     avatar.classList.add('focused');
-    console.log('Клик по изображению');
   });
 });
 
+
+
+const myVideo = document.querySelector(".myVideo");
+const toggleBtn = document.querySelector(".togglePlay");
+
+let isPlaying = false;
+
+myVideo.onclick = () => {
+
+  if (!isPlaying) {
+    myVideo.play();
+  } else {
+    myVideo.pause();
+  }
+  isPlaying = !isPlaying; 
+}
+
+myVideo.onplay = () => {
+isPlaying = false;
+};
+
+myVideo.onpause = () => {
+  isPlaying = true;
+};
